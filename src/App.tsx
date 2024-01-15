5df5384c71868ad8777759e31ca3f234bf2a8678
dstrivedi <drashti1118@gmail.com>
2024-01-14 22:46:45 -0500

    useSWRInfinite

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useState } from 'react';
 import { Pokemon } from './Pokemon';
 import { useRequest } from './useRequest';
+import useSWRInfinite from "swr/infinite";
 
 import './style.css';
 
 export const App:FC = ():JSX.Element => {
-  const {data: result, error, isLoading} = useRequest('/pokemon')
+  const [count, setCount] = useState(0);
+
+  const {data:result, mutate, size, setSize, isValidating, error, isLoading} = useSWRInfinite((index) => `https://pokeapi.co/api/v2/pokemon?page=${index+1}`);
+  // const {data: result, error, isLoading} = useRequest('/pokemon')
   if(error) return (<p>An error is occured.</p>);
   if (isLoading) return (<p>"Loading..."</p>); 
   return (
     <main className='App'>
       <h1>Pokedex</h1>
       <div>
-          {result.results.map((pokemon) => (
+          {result.map((pokemon) => (
               <Pokemon key={pokemon.name} pokemon={pokemon} />
           ))}
       </div>
+      <button onClick={() => setSize(1)}>
+          Load more...
+      </button>
     </main>
   );
 };
---
+++ src/usePagination.tsx
@@ -0,0 +1,6 @@
+import useSWR from "swr";
+import { Pokemon } from "./Pokemon";
+
+export const usePagination = (path) => {
+    
+}
