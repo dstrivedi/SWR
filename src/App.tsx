f48923c923da7b103b54b10cf9fda58cb594f168
dstrivedi <drashti1118@gmail.com>
2024-01-24 09:11:06 -0500

    promise.all

--- src/App.tsx
+++ src/App.tsx
@@ -6,31 +6,35 @@ import useSWRInfinite from 'swr/infinite';
 
 import './style.css';
 
-const fetcher = (url) => fetch(url).then((res) => res.json()).then((data) => data.results);
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export const App:FC = ():JSX.Element => {
   const {data, size, setSize, error, isLoading} = useSWRInfinite((index) => `https://pokeapi.co/api/v2/pokemon?offset=${index*2*10}&limit=10`, fetcher, {initialSize: 1, revalidateFirstPage: false});
-  const [pokemons, setPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState<Array<any>>([]);
+
   if(error) return (<p>An error is occured.</p>);
   if (isLoading) return (<p>"Loading..."</p>);
 
-  useEffect(() => {
-    setPokemons((prevPokemons) => [...prevPokemons, ...data[0]]);
-  }, [data])
+    // Use a single useEffect for setting Pokemon data
+    useEffect(() => {
+      if (data) {
+        const fetchData = async () => {
+          const detailedData = await Promise.all(
+            data.flatMap((page) => page.results.map((pokemon) => fetcher(pokemon.url)))
+          );
+          setPokemons((prevPokemons) => [...prevPokemons, ...detailedData]);
+        };
+  
+        fetchData();
+      }
+    }, [data]);
 
   return (
     <main className='App'>
       <h1>Pokedex Page {size}</h1>
       <div>
           {pokemons.map((pokemon) => (
-              // <Pokemon key={pokemon.name} pokemon={pokemon} />
-              <div className='Card' key={pokemon.name}>
-                <span>{pokemon.name}</span>
-                {/* <span className='Card--id'>#{pokemon.id}</span> */}
-                {/* <img className='Card--image' src={data.sprites.front_default} alt={name} />
-                <h1 className='Card--name'>{name}</h1>
-                <span className='Card--details'>{data.types.map((poke) => poke.type.name)}</span> */}
-            </div>
+              <Pokemon key={pokemon.name} pokemon={pokemon} />
           ))}
       </div>
       <button onClick={() => setSize(size+1)}>
